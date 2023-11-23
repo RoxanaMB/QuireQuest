412408ee0fea495863d0d5603942fcb6bf94da3b
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-11-23 22:12:32 +0000

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthComponent } from './quire-quest/pages/auth/auth.component';
+import { AccountComponent } from './quire-quest/pages/account/account.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
