5b717dd44497dc969b299b1d32e72ad0f75d0991
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2023-11-24 21:18:26 +0000

    Login & Sign Up html - Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthComponent } from './quire-quest/pages/auth/auth.component';
 import { AccountComponent } from './quire-quest/pages/account/account.component';
+import { LoginPageComponent } from './quire-quest/pages/login-page/login-page.component';
+import { SignUpPageComponent } from './quire-quest/pages/sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    AccountComponent
+    AccountComponent,
+    LoginPageComponent,
+    SignUpPageComponent,
   ],
   imports: [
     BrowserModule,
