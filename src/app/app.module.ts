ddb3451f20cc4ffb99fbbe84fa339c32463846f4
RoxanaMB <alu0101339887@ull.edu.es>
2024-01-10 20:42:52 +0000

    Add FormsModule and HttpClientModule to app.module.ts, and implement user registration functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +34,9 @@ import { DescriptionComponent } from './quire-quest/components/description/descr
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quire-quest/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/quire-quest/pages/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up-page.component.css']
 })
 export class SignUpPageComponent {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+
+  constructor(public userService: UsersService) {
+    this.username = "";
+    this.email = "";
+    this.password = "";
+    this.confirmPassword = "";
+  }
+
+  register() {
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+    this.userService.register(user).subscribe((response: {token: string}) => {
+      const token = response.token;
+      if (token) {
+        localStorage.setItem("token", token);
+        window.location.href = "/login";
+      }
+    });
+  }
 
 }
