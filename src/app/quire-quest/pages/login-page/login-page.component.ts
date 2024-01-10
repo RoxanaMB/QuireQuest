1b8f0d143e9ae4d4bd1869b6f2e8082354998644
RoxanaMB <alu0101339887@ull.edu.es>
2024-01-10 20:56:09 +0000

    Add ngModel bindings and login functionality to login-page component

--- src/app/quire-quest/pages/login-page/login-page.component.ts
+++ src/app/quire-quest/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  email: string;
+  password: string;
 
+  constructor(public userService: UsersService) {
+    this.email = "";
+    this.password = "";
+  }
+
+  login() {
+    const user = {
+      email: this.email,
+      password: this.password
+    };
+    this.userService.login(user).subscribe((response: {token: string}) => {
+      const token = response.token;
+      console.log(token);
+      if (token) {
+        localStorage.setItem("token", token);
+        window.location.href = "/account";
+      }
+    });
+  }
 }
