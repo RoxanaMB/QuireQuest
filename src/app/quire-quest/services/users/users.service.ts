372a01ebc6037399c80a83dee89db65ef9f80f6a
RoxanaMB <alu0101339887@ull.edu.es>
2024-05-14 19:32:43 +0100

    Update API URLs to use HTTP instead of HTTPS

--- src/app/quire-quest/services/users/users.service.ts
+++ src/app/quire-quest/services/users/users.service.ts
@@ -10,15 +10,15 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   login(user: any): Observable<any> {
-    return this.http.post("https://qq-back.vercel.app/login", user);
+    return this.http.post("http://qq-back.vercel.app/login", user);
   }
 
   register(user: any): Observable<any> {
-    return this.http.post("https://qq-back.vercel.app/register", user);
+    return this.http.post("http://qq-back.vercel.app/register", user);
   }
 
   getUser(token: string): Observable<any> {
-    return this.http.get("https://qq-back.vercel.app/user", {
+    return this.http.get("http://qq-back.vercel.app/user", {
       headers: {
         'x-access-token': token
       }
